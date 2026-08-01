f25031cf5b32639e80ea3bbba4634eec17b50875
Sim-M <40467906+Sim-M@users.noreply.github.com>
2026-08-01 15:53:49 +0530

    Update note.model.js

--- app/models/note.model.js
+++ app/models/note.model.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose');
 const NoteSchema = mongoose.Schema({
     title: String,
     content: String
+    tags: {
+    type: [String],
+    default: []
+  }
+    
 }, {
     timestamps: true
 });
